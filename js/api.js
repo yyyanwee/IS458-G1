a2393f77424c93a8658ff8ab23f5f0a6c5d6318f
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-04-02 21:50:16 +0800

    asd

---
+++ js/api.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const axios = require('axios');
+
+const url = 'https://t1tvpydrmk.execute-api.ap-southeast-1.amazonaws.com/production';
+
+const requestBody = 
+{
+    "Item":{
+      "Id": 99,
+      "Title": "Book 99 Title",
+      "ISBN": "999-9999999999",
+      "Authors": [
+        "Author99"
+      ],
+      "Price": 99,
+      "Dimensions": "8.5 x 11.0 x 0.5",
+      "PageCount": 500,
+      "InPublication": false,
+      "ProductCatalog": "Book"  
+    }
+};
+
+const config = {
+  method: 'put',
+  url: url,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  data: requestBody,
+};
+
+axios(config)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
